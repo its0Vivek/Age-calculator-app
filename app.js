d54776680691e71f6bcba6c723c97f5934a431b7
Vivek Mali <148007729+its0Vivek@users.noreply.github.com>
2024-03-05 18:58:00 +0530

    a

---
+++ app.js
@@ -0,0 +1,10 @@
+let day = document.querySelector("#day");
+// let month = document.querySelector("");
+// let year = document.querySelector("");
+
+
+day.addEventListener("input", function () {
+    console.log(day.value)
+})
+
+
